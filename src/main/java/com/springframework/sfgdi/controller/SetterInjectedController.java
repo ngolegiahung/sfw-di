b22815b48be555002001ecf7bb5553c6d0952981
Coroutine <hung.l.g.ngo@gmail.com>
2024-05-14 22:28:03 +0700

    Using qualifier to get correct DI when have many Service same Interface.

--- src/main/java/com/springframework/sfgdi/controller/SetterInjectedController.java
+++ src/main/java/com/springframework/sfgdi/controller/SetterInjectedController.java
@@ -2,6 +2,7 @@ package com.springframework.sfgdi.controller;
 
 import com.springframework.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +11,7 @@ public class SetterInjectedController {
     public GreetingService greetingService;
 
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/test/java/com/springframework/sfgdi/controller/SetterInjectedControllerTest.java
+++ src/test/java/com/springframework/sfgdi/controller/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package com.springframework.sfgdi.controller;
 
-import com.springframework.sfgdi.services.GreetingServiceImpl;
+import com.springframework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -14,7 +12,7 @@ class SetterInjectedControllerTest {
     void setUp() {
         controller = new SetterInjectedController();
 
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test
