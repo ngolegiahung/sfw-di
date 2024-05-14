08392d677dd2076d4b821ee8eac7c851fa23d9d6
Coroutine <hung.l.g.ngo@gmail.com>
2024-05-14 21:44:14 +0700

    Add manual Di example - property, setter, constructor.

---
+++ src/main/java/com/springframework/sfgdi/services/GreetingServiceImpl.java
@@ -0,0 +1,9 @@
+package com.springframework.sfgdi.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/springframework/sfgdi/controller/SetterInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package com.springframework.sfgdi.controller;
+
+import com.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+    }
+}
